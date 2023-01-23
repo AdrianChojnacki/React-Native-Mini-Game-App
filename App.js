208f3d863402d012c45ff362fb5b261d7c9e3ddc
Adrian Chojnacki <adrian.chojnacki@frontkom.com>
2023-01-23 14:08:50 +0100

    Added gradient

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import { View, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f",
   },
 });
