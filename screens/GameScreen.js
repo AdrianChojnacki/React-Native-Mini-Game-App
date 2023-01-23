021202681b6e00d5dd6812bb1efa5f9a88151155
Adrian Chojnacki <adrian.chojnacki@frontkom.com>
2023-01-23 15:18:19 +0100

    Added GameScreen component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,24 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 function GameScreen() {
-  return <Text>GameScreen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
