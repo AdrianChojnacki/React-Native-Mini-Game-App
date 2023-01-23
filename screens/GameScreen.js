05a45edd276513464c6fbf783d1faa5bae3a8695
Adrian Chojnacki <adrian.chojnacki@frontkom.com>
2023-01-23 15:13:10 +0100

    Added App logic

---
+++ screens/GameScreen.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function GameScreen() {
+  return <Text>GameScreen</Text>;
+}
+
+export default GameScreen;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   function numberInputHandler(enteredText) {
@@ -25,7 +25,7 @@ function StartGameScreen() {
       return;
     }
 
-    console.log("Valid number!");
+    onPickNumber(chosenNumber);
   }
 
   return (
