8f7351c736e24be5d9cbb838a6bbdf651a1d98d4
Adrian Chojnacki <adrian.chojnacki@frontkom.com>
2023-01-24 18:24:38 +0100

    Added data logic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -30,6 +30,11 @@ function GameScreen({ userNumber, onGameOver }) {
     }
   }, [currentGuess, userNumber, onGameOver]);
 
+  useEffect(() => {
+    minBoundary = 1;
+    maxBoundary = 100;
+  }, []);
+
   function nextGuessHandler(direction) {
     if (
       (direction === "lower" && currentGuess < userNumber) ||
