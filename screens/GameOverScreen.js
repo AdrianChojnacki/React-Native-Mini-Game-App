c9fc98939eda211485948dca5e504a671fb2b6f0
Adrian Chojnacki <adrian.chojnacki@frontkom.com>
2023-01-24 18:11:54 +0100

    Added GameOverScreen styles

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,42 @@
-import { Text } from "react-native";
+import { View, Image, StyleSheet, Text } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 function GameOverScreen() {
-  return <Text>Game is over</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/images/success.png")}
+          style={styles.image}
+        />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y</Text>
+    </View>
+  );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    padding: 24,
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    margin: 36,
+    borderWidth: 3,
+    borderColor: Colors.primary700,
+    borderRadius: 150,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
