7575d1553fc4191179230f0fdf5442968c80f8ab
Adrian Chojnacki <adrian.chojnacki@frontkom.com>
2023-01-24 18:34:23 +0100

    Added Flatlist

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -84,9 +84,11 @@ function GameScreen({ userNumber, onGameOver }) {
         </View>
       </Card>
       <View>
-        {/* {guessRounds.map((guessRound) => (
-          <Text key={guessRound}>{guessRound}</Text>
-        ))} */}
+        <FlatList
+          data={guessRounds}
+          renderItem={(itemData) => <Text>{itemData.item}</Text>}
+          keyExtractor={(item) => item}
+        />
       </View>
     </View>
   );
