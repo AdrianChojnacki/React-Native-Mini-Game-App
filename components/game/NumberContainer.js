092d87f0aee4936a039c1ffba645fb0bdb717103
Adrian Chojnacki <adrian.chojnacki@frontkom.com>
2023-01-26 18:49:18 +0100

    Added Dimensions styling

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
 import Colors from "../../constants/colors";
 
 function NumberContainer({ children }) {
@@ -11,12 +11,14 @@ function NumberContainer({ children }) {
 
 export default NumberContainer;
 
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     justifyContent: "center",
-    margin: 24,
-    padding: 24,
+    margin: deviceWidth < 380 ? 12 : 24,
+    padding: deviceWidth < 380 ? 12 : 24,
     borderWidth: 4,
     borderColor: Colors.accent500,
     borderRadius: 8,
@@ -24,6 +26,6 @@ const styles = StyleSheet.create({
   numberText: {
     color: Colors.accent500,
     fontFamily: "open-sans-bold",
-    fontSize: 36,
+    fontSize: deviceWidth < 380 ? 28 : 36,
   },
 });
